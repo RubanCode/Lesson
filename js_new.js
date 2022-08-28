17a97f579432fde69adbaeed8df4836f04bcb3e6
Ruban Pavel <ruban.pavel@outlook.com>
2022-08-28 09:31:09 +0300

    base js 1

--- js_new.js
+++ js_new.js
@@ -8,3 +8,23 @@ fact = "is awesome!";
 console.log(FCC, fact);
 
 const sum = 10 + 10;
+
+const difference = 45 - 33;
+
+const product = 8 * 10;
+
+const quotient = 66 / 33;
+
+let myVar = 87;
+
+// Only change code below this line
+myVar = myVar++ + 1;
+
+let myVar = 11;
+
+// Only change code below this line
+myVar = myVar-- - 1;
+
+const myDecimal = Number(5.7);
+
+const product = 2.0 * 2.5;
